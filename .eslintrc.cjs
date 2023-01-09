8447d27769aeef1986a19bafdac50d95e78a9bec
WrappedUsername <wrappedusername@gmail.com>
2023-01-08 22:32:06 -0700

    Added eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: false
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
